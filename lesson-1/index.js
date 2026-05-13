e7db4842854b7dfc0ae5037e46bc5404acf86d19
Ksenia <galich.ksyu98@gmail.com>
2026-05-13 14:19:02 +0500

    task js 3

--- lesson-1/index.js
+++ lesson-1/index.js
@@ -1,21 +1,63 @@
 // Задача 1
-for (let i = 1; i <= 5; i++) {
-  console.log("Привет, мир!");
+function sayHello(name) {
+  console.log("Привет, " + name + "!");
 }
+
+sayHello("Аня");
 
 // Задача 2
-for (let i = 1; i <= 10; i++) {
-  console.log(i);
+function square(num) {
+  return num * num;
 }
+
+let result1 = square(2);
+let result2 = square(5);
+let result3 = square(10);
+
+console.log(result1);
+console.log(result2);
+console.log(result3);
 
 // Задача 3
-for (let i = 2; i <= 20; i += 2) {
-  console.log(i);
+function isBigger(a, b) {
+  return a > b;
 }
 
-// Задача 4
-let sum = 0;
-for (let i = 1; i <= 100; i++) {
-  sum += i;
+console.log(isBigger(10, 5));
+console.log(isBigger(2, 8));
+
+//Задача 4
+function sayHi() {
+  console.log("Привет!");
 }
-console.log(sum);
+
+function doTwice(callback) {
+  callback();
+  callback();
+}
+
+doTwice(sayHi);
+
+//Задача 5
+function formatName(name) {
+  return "Пользователь: " + name;
+}
+
+function greet(name) {
+  console.log("Добро пожаловать, " + formatName(name));
+}
+
+greet("Лена");
+
+//Задача 6
+function createMultiplier(multiplier) {
+  return function (num) {
+    return num * multiplier;
+  };
+}
+
+const double = createMultiplier(2);
+console.log(double(5));
+
+const triple = createMultiplier(3);
+console.log(triple(4));
