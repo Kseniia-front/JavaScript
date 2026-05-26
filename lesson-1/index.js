e68e74ad75ee92b168bc4e6fc5bd3d19c8d92779
Ksenia <galich.ksyu98@gmail.com>
2026-05-26 21:09:23 +0500

    done task js 6

--- lesson-1/index.js
+++ lesson-1/index.js
@@ -1,97 +1,74 @@
-// Задача 1 - Добавление элемента
-// Создай массив fruits
-// Создай функцию aadFruit(arr, fruit)
-// Сохрани результат вызова функции в переменную и выведи ее в консоль
-const fruits = ["banana", "orange"];
-function addFruit(arr, fruit) {
-  arr.push(fruit);
-  return arr;
-}
-const result = addFruit(fruits, "apple");
-console.log(result);
+// Задача 1 - с помощью метода массива выведи в консоль значение каждого элемента
+const names = ["Alice", "Bob", "Charlie"];
+names.forEach(function (name) {
+  console.log(name);
+});
 
-// Задача 2 - Удаление последнего
-//Создай массив numbers
-//Создай функцию removeLast(arr)
-const numbers = [10, 20, 30, 40];
-function removeLast(arr) {
-  return arr.pop();
-}
-const removed = removeLast(numbers);
-console.log(removed);
-console.log(numbers);
+// Задача 2 - Выведи в консоль имена с порядковым номером (начиная с 1)
+names.forEach(function (name, index) {
+  console.log(index + 1 + ". " + name);
+});
 
-// Задача 3 - Проверка наличия
-// Создай массив users
-//Создай функцию hasUser(arr, name)
-const users = ["Alex", "Anna", "Max"];
-function hasUser(arr, name) {
-  return arr.includes(name);
-}
-const exists = hasUser(users, "Alex");
-console.log(exists);
+// Задача 3 - Массив заказов
+// Заказ 101 laptop
+// Заказ 102 Phone
+// ...
+const orders = [
+  { id: 101, product: "Laptop" },
+  { id: 102, product: "Phone" },
+  { id: 103, product: "Tablet" },
+];
+orders.forEach(function (order) {
+  console.log("Заказ #" + order.id + ": " + order.product);
+});
 
-// Задача 4 - Получить индекс
-// Создай массив colors
-// Создай функцию getIndex(arr, color)
-const colors = ["red", "blue", "green"];
-function getIndex(arr, color) {
-  return arr.indexOf(color);
-}
-const index = getIndex(colors, "red");
-console.log(index);
+// Задача 4 - Удвой все числв в массиве
+// Верни новый массив: 2, 4, 6
+const nums = [1, 2, 3];
+const doubledNums = nums.map(function (num) {
+  return num * 2;
+});
+console.log(doubledNums);
 
-// Задача 5 - Добавление в начало
-// Создай массив tasks
-// Создай функцию addTaskToStart(arr, task)
-const tasks = ["Помыть посуду", "Сделать уроки"];
-function addTaskToStart(arr, task) {
-  arr.unshift(task);
-  return arr;
-}
-const updatedTasks = addTaskToStart(tasks, "Сделать ДЗ");
-console.log(updatedTasks);
+// Задача 5 - Из массива имён создай массив объектов
+const arrayNames = ["Alice", "Bob"];
+const namesInfo = arrayNames.map(function (name) {
+  return {
+    name: name,
+    length: name.length,
+  };
+});
+console.log(namesInfo);
 
-// Задача 6 - Удаление первого элемента
-// Создай массив messages
-// Создай функцию RemoveFirst(arr)
-const messages = ["Привет", "Как дела?", "Пока"];
-function removeFirst(arr) {
-  return arr.shift();
-}
-const first = removeFirst(messages);
-console.log(first);
-console.log(messages);
+// Задача 6 - У тебя массив пользователей users
+// Создай новый массив строк вида Alice 20 лет, Bob 17лет
+const users = [
+  { name: "ALice", age: 20 },
+  { name: "Bob", age: 17 },
+];
+const userStrings = users.map(function (user) {
+  return user.name + " (" + user.age + " лет)";
+});
+console.log(userStrings);
 
-// Задача 7 - Преобразование в строку
-// Создай массив words
-// Создай функцию makeSentence(arr)
-const words = ["Привет", "мой", "друг"];
-function makeSentence(arr) {
-  return arr.join(" ");
-}
-const sentence = makeSentence(words);
-console.log(sentence);
+// Задача 7 - Отфильтровать пользователей старше 18 лет
+const dataUsers = [
+  { name: "Alice", age: 20 },
+  { name: "Bob", age: 17 },
+  { name: "Charlie", age: 25 },
+];
+const adultUsers = dataUsers.filter(function (user) {
+  return user.age > 18;
+});
+console.log(adultUsers);
 
-// Задача 8 - Количество элементов
-// Создай массив items
-// Создай функцию getCount(arr)
-const items = ["книга", "ручка", "тетрадь"];
-function getCount(arr) {
-  return arr.length;
-}
-const count = getCount(items);
-console.log(count);
-
-// Задача 9 - Уникальность элемента
-// Создай массив emails
-// Создай фукнцию addEmail(arr, email)
-const emails = ["hello@mail.com", "user@mail.com"];
-function addEmail(arr, email) {
-  if (!arr.includes(email)) {
-    arr.push(email);
-  }
-  return arr;
-}
-const updatedEmails = addEmail(emails, "test@maol.com");
-console.log(updatedEmails);
+// Задача 8 - У тебя массив заказов, нужно оставить только те, где статус - delivered
+const dataOrders = [
+  { id: 1, status: "delivered" },
+  { id: 2, status: "pending" },
+  { id: 3, status: "delivered" },
+];
+const deliveredOrders = dataOrders.filter(function (order) {
+  return order.status === "delivered";
+});
+console.log(deliveredOrders);
